3c64a20bc5d8dc80cba380b6b06d33fdc6c244c0
Cateiru <yuto.w51942@gmail.com>
2024-06-14 15:15:55 +0900

    feat: Add support for replace mode in comment logic

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
-import { normalMode, recreateMode, appendMode, deleteMode } from './modes';
+import {
+  normalMode, recreateMode, appendMode, deleteMode, replaceMode,
+} from './modes';
 import { getCommenter } from './comment/commenter';
 
 (async () => {
@@ -14,12 +16,14 @@ import { getCommenter } from './comment/commenter';
     const number = core.getInput('number');
     const commitSHA = core.getInput('commit-sha');
     const identifier = core.getInput('id');
-    const appendComment = core.getInput('append');
-    const recreateComment = core.getInput('recreate');
-    const deleteComment = core.getInput('delete');
     const fail = core.getInput('fail');
     const githubToken = core.getInput('github-token');
     const message = core.getInput('message');
+
+    const appendComment = core.getInput('append');
+    const replaceComment = core.getInput('replace');
+    const recreateComment = core.getInput('recreate');
+    const deleteComment = core.getInput('delete');
 
     const octokit = github.getOctokit(githubToken);
 
@@ -36,24 +40,37 @@ import { getCommenter } from './comment/commenter';
       return;
     }
 
-    let mode = normalMode;
+    const effectiveModes = [
+      ['recreate', recreateComment],
+      ['append', appendComment],
+      ['replace', replaceComment],
+      ['delete', deleteComment],
+    ].filter((i) => i === 'true');
 
-    if (appendComment === 'true' && recreateComment === 'true') {
-      core.setFailed('Not allowed to set both `append` and `recreate` to true.');
+    if (effectiveModes.length > 1) {
+      const effectiveModeNames = effectiveModes.map((i) => i[0]).join(', ');
+      core.setFailed(`Only one of ${effectiveModeNames} can be set to true.`);
       return;
     }
 
-    if (deleteComment === 'true' && (appendComment === 'true' || recreateComment === 'true')) {
-      core.setFailed('Not allowed to set `delete` to true with `append` or `recreate`.');
-      return;
-    }
+    let mode;
 
-    if (recreateComment === 'true') {
-      mode = recreateMode;
-    } else if (appendComment === 'true') {
-      mode = appendMode;
-    } else if (deleteComment === 'true') {
-      mode = deleteMode;
+    switch (effectiveModes[0][0]) {
+      case 'recreate':
+        mode = recreateMode;
+        break;
+      case 'append':
+        mode = appendMode;
+        break;
+      case 'replace':
+        mode = replaceMode;
+        break;
+      case 'delete':
+        mode = deleteMode;
+        break;
+      default:
+        mode = normalMode;
+        break;
     }
 
     await mode(commenter, identifier, message);
